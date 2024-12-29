43912685f1b1110ea3c7e63381c5bbb40960f9f7
Gabriel Frias <maddaxzyt@gmail.com>
2024-12-29 03:06:58 -0300

    Delete src directory

--- src/services/userService.ts
+++
@@ -1,8 +0,0 @@
-import { useAuthStore } from '../store/useAuthStore';
-
-export const userService = {
-  getUserByEmail: (email: string) => {
-    const users = useAuthStore.getState().users;
-    return users.find(user => user.email === email);
-  },
-};
