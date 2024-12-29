b558dc3188d895f9098e9ba82c9029aa171624c5
Gabriel Frias <maddaxzyt@gmail.com>
2024-12-29 03:26:32 -0300

    Delete src directory

--- src/utils/dateFormat.ts
+++
@@ -1,8 +0,0 @@
-export const formatDateBR = (date: string | Date) => {
-  const d = new Date(date);
-  return d.toLocaleDateString('pt-BR', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric'
-  });
-};
